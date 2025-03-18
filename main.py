c1164f00535d7abd30033ada401792d06781febd
MMW BOTZ <155316405+mmwbotzmain@users.noreply.github.com>
2025-03-18 21:20:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-#=========================================================================
-# [AutoDelete - Telegram bot to delete messages after specific time]      
-# Copyright (C) 2022 Arunkumar Shibu                       
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#=========================================================================
-
 from time import time 
 from utils.info import *
 from utils.database import *
@@ -42,8 +24,6 @@ async def delete(user, message):
 @User.on_message(filters.regex("!start") & filters.private)
 async def start(user, message):
     await message.reply("Hi, I'm alive!")
-
-#==========================================================
 
 Popen(f"gunicorn utils.server:app --bind 0.0.0.0:{PORT}", shell=True)
 Popen("python3 -m utils.delete", shell=True)
